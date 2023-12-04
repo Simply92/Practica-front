0e15403c13aaff7fe19c2370945b9a1ab04caf95
Simply92 <pedrouner92@gmail.com>
2023-12-04 18:59:39 -0300

    primero

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ResponsiveAppBar from "./componets/NavBar/NavBar";
+import SearchAppBar from "./componets/searchBar/SearchBar"
+
+function App() {
+  return (
+    <>
+      <SearchAppBar/>
+      <ResponsiveAppBar/>
+    </>
+  );
+}
+
+export default App;
